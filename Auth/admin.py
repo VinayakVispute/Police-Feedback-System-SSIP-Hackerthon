8ad2f9bdef4aa237abffdf1acb612eee3b924156
Vinayak Vispute <93467074+VinayakVispute@users.noreply.github.com>
2022-12-16 03:29:41 +0530

    Updated Auth Module Admin Panel
    Removed Session Module from Admin Panel means deregistered it

--- Auth/admin.py
+++ Auth/admin.py
@@ -1,16 +1,3 @@
 from django.contrib import admin
-from django.contrib.sessions.models import Session
-import pprint
-
-
-class SessionAdmin(admin.ModelAdmin):
-    def _session_data(self, obj):
-        return pprint.pformat(obj.get_decoded()).replace('\n','<br>\n')
-    _session_data.allow_tags=True
-    list_display = ['session_key', '_session_data', 'expire_date']
-    readonly_fields = ['_session_data']
-    exclude = ['session_data']
-    
-admin.site.register(Session, SessionAdmin)
 
 # Register your models here.
